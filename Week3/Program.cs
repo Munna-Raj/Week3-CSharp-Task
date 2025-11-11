e115c3c6a719d6a2939dd467c50fe61dd79384c0
Munna-Raj <my2056875@gmail.com>
2025-11-11 10:05:52 +0545

    task 4 completed

--- Week3/Program.cs
+++ Week3/Program.cs
@@ -13,9 +13,16 @@ namespace Week3
             op.Multiply(6, 2);
             op.Divide(6, 2);
             op.OddEvenFinder(7);
+            
 
             Console.WriteLine("\n--- Running Task2_NullOperations ---");
             Task2_NullOperations.Run();
+
+            Console.WriteLine("\n--- Running Task3_AgeCheck ---");
+            Task3_AgeCheck.Run();
+
+            Console.WriteLine("\n--- Running Task4_DaySwitch ---");
+            Task4_DaySwitch.Run();   
         }
     }
 
@@ -69,6 +76,70 @@ namespace Week3
 
             username ??= "DefaultUser";
             Console.WriteLine("Updated Username: " + username);
+        }
+    }
+
+    internal class Task3_AgeCheck
+    {
+        public static void Run()
+        {
+            Console.Write("\nEnter your age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age < 13)
+            {
+                Console.WriteLine("Child");
+            }
+            else if (age < 20)
+            {
+                Console.WriteLine("Teenager");
+            }
+            else if (age < 60)
+            {
+                Console.WriteLine("Adult");
+            }
+            else
+            {
+                Console.WriteLine("Senior Citizen");
+            }
+        }
+    }
+
+    
+    internal class Task4_DaySwitch
+    {
+        public static void Run()
+        {
+            Console.Write("\nEnter a number (1–7): ");
+            int day = int.Parse(Console.ReadLine());
+
+            switch (day)
+            {
+                case 1:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 2:
+                    Console.WriteLine("Monday");
+                    break;
+                case 3:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 5:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 6:
+                    Console.WriteLine("Friday");
+                    break;
+                case 7:
+                    Console.WriteLine("Saturday");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Number! Enter between 1–7.");
+                    break;
+            }
         }
     }
 }
